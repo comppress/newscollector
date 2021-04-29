157cd3f90579665e95646fec14345dae5b6bb296
Lucas Dario Stocksmeier <l.stocksmeier@campus.tu-berlin.de>
2021-04-29 15:46:17 +0200

    configure csrf and user

--- src/main/java/org/example/NewsCollector/security/SecurityConfig.java
+++ src/main/java/org/example/NewsCollector/security/SecurityConfig.java
@@ -1,4 +1,41 @@
 package org.example.NewsCollector.security;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Value("${spring.security.admin.password}")
+    String adminPassword;
+
+    @Value("${spring.security.user.password}")
+    String userPassword;
+
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode(userPassword)).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode(adminPassword)).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").hasRole("USER").and().formLogin().and().csrf().disable();
+    }
 }
